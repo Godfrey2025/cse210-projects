017dc9f40d9f63b9a1d538e435f6a3f1787357cd
godfreygova <ggova@byupathway.edu>
2025-09-05 09:44:57 +0200

    exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Console.WriteLine("What is the magic number?");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        int userGuess = -1;
+
+        while (userGuess != magicNumber)
+        {
+            Console.WriteLine("Guess the magic number:");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the magic number.");
+            }
+        }
     }
 }
