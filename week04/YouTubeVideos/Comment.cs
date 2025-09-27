ec6c6c82b362ba1cb3623bcc32617f6e2644f990
godfreygova <ggova@byupathway.edu>
2025-09-27 12:40:55 +0200

    youtube videos

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,12 @@
+// Comment class
+public class Comment
+{
+    public string CommenterName { get; }
+    public string Text { get; }
+
+    public Comment(string name, string text)
+    {
+        CommenterName = name;
+        Text = text;
+    }
+}
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,51 +1,6 @@
 using System;
 using System.Collections.Generic;
 
-// Comment class
-class Comment
-{
-    public string CommenterName { get; set; }
-    public string Text { get; set; }
-
-    public Comment(string name, string text)
-    {
-        CommenterName = name;
-        Text = text;
-    }
-}
-
-// Video class
-class Video
-{
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public int LengthInSeconds { get; set; }
-    private List<Comment> comments;
-
-    public Video(string title, string author, int length)
-    {
-        Title = title;
-        Author = author;
-        LengthInSeconds = length;
-        comments = new List<Comment>();
-    }
-
-    public void AddComment(Comment comment)
-    {
-        comments.Add(comment);
-    }
-
-    public int GetCommentCount()
-    {
-        return comments.Count;
-    }
-
-    public List<Comment> GetComments()
-    {
-        return comments;
-    }
-}
-
 // Main program
 class Program
 {
@@ -61,14 +16,14 @@ class Program
         videos.Add(video1);
 
         // Create Video 2
-        Video video2 = new Video("Programming with C# for Beginners", "CodeAcademy", 900);
-        video2.AddComment(new Comment("Emma", "Loved the class."));
-        video2.AddComment(new Comment("Oliver", "It was very informative."));
-        video2.AddComment(new Comment("Ava", "Thank you for the clear explanations!"));
+        Video video2 = new Video("DIY Home Office Setup", "TechSavvy", 480);
+        video2.AddComment(new Comment("Emma", "Loved the cable management tips."));
+        video2.AddComment(new Comment("Oliver", "Where did you get that desk?"));
+        video2.AddComment(new Comment("Ava", "Super helpful for small spaces."));
         videos.Add(video2);
 
         // Create Video 3
-        Video video3 = new Video("Online Yoga Session", "HealthyLiving", 600    );
+        Video video3 = new Video("Beginner Yoga Routine", "ZenLife", 600    );
         video3.AddComment(new Comment("Mason", "Perfect for my morning routine."));
         video3.AddComment(new Comment("Isabella", "So calming and easy to follow."));
         video3.AddComment(new Comment("Lucas", "Please make a part 2!"));
